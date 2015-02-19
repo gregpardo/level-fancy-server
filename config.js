69c889fbdc5225056a09326cfa58b3a74ca56d11
Greg Pardo <greg.pardo@tesseractsensors.com>
2015-02-19 16:12:06 -0500

    Use different defaults for possible docker config

--- config.js
+++ config.js
@@ -4,8 +4,8 @@
 module.exports = {
   port:97892
   ,name:'level fancy server'
-  ,manifest:'/tmp/fancy-manifest.json'
-  ,database:'./database'    //location of database folder
+  ,manifest:'/data/config/fancy-manifest.json'
+  ,database:'/data/database'    //location of database folder
   ,timeout:0 //will disconnect a client in X milliseconds of inactivity, 0 disables
   //passes configuration options when starting leveldb
   ,options:{
@@ -31,17 +31,17 @@ module.exports = {
         name:'root'
         ,pass:'toor'
       }
-      
+
     ]
   }
   //Define sublevels and secondary indexes here. Follow example conventions.
-  //Uses sublevel and level-sec: 
+  //Uses sublevel and level-sec:
   //https://github.com/dominictarr/level-sublevel
   //https://github.com/juliangruber/level-sec
   ,sublevels:[
     {
       //sublevel db.posts with 3 secondary indices
-      name:'posts' 
+      name:'posts'
       ,indices:[
         { name:'Title' ,keys:['title'] } //db.posts.byTitle
         ,{ name:'Length' ,keys:['body.length'] } //db.posts.byLength
